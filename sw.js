9d1554184cf885aec32f61a84746854df48e3f1a
clicianoxsilva-web <clicianoxsilva@gmail.com>
2026-08-31 00:18:22 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE='prbbl-pwa-v5.3';
+const CORE=['./','./index.html','./manifest.webmanifest','./config.js','./icon-96.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png','./favicon-32.png','./favicon-48.png','./offline.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==self.location.origin)return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./offline.html'))));
+});
