107f6afedaac23eeb329b4eeda660cfec213ac2a
clicianoxsilva-web <clicianoxsilva@gmail.com>
2026-08-31 01:22:18 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.PRBBL_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyLaikftXgJoIfVw7M8L7_0NhMDao08-LvYJfePJm7byK2RQ8X142Tebi8ebif0okFf/exec';
+window.PRBBL_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyqVLdDfprFcXsUZs-qeJZyJyXMXwT9ET699vJ7tZp5rZuyD8g8QoNMA1PXROBkOerbjQ/exec';
